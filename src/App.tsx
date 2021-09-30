191e2e607c5a0dc3f50e343a4b237dc46ab91067
zhaoxuan <zhaoxuan@boo.chat>
2021-09-30 14:30:13 +0800

    dummy-puggy

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,45 @@
 import React from 'react';
+import { 
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+import Footer from './components/footer/footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <h1 style={{'fontSize': "60px"}}>shouye </h1>
+      <div className="header">
+        <div className="container">
+          <div className="flex align-center pad3">
+            <Link to="/"><h1 className="logo">DummyPuggy</h1></Link> 
+            <div className="spec"></div>
+            <ul className='navList'>
+              <li className="item flex align-center">
+                <a href="https://twitter.com/DummyPuggy" target="_blank" rel="noreferrer">
+                  <img src={require('./images/twitterlogo.png').default} alt="" width="32"/>
+                </a>
+              </li>
+              <li className="item flex align-center">
+                <a href="https://discord.gg/mgmMQ9w3D5" target="_blank" rel="noreferrer">
+                  <img src={require('./images/Discord_Inverted_Icon.png').default} alt="" width="32"/>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
+      <Switch>
+        <Route exact path="/">
+          <div className="main">
+            <img src={require('./images/coming.gif').default} alt="" width="100%"/>
+          </div>
+        </Route>
+      </Switch>
+      <Footer/>
+      
     </div>
   );
 }
-
 export default App;
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link,} from 'react-router-dom';
+import './footer.css';
+
+
+const Footer : React.FC = (props)=> {
+  return (
+    <footer>
+      <div className="container">
+          <div className="flex align-center">
+            <Link to="/"><h1 className="logo">DummyPuggy</h1></Link> 
+            <div className="spec"></div>
+            <ul className='navList'>
+              <li className="item flex align-center">
+                <a href="https://twitter.com/DummyPuggy" target="_blank" rel="noreferrer">
+                  <img src={require('../../images/twitterlogo.png').default} alt="" width="32"/>
+                </a>
+              </li>
+              <li className="item flex align-center">
+                <a href="https://discord.gg/mgmMQ9w3D5" target="_blank" rel="noreferrer">
+                  <img src={require('../../images/Discord_Inverted_Icon.png').default} alt="" width="32"/>
+                </a>
+              </li>
+            </ul>
+          </div>
+          </div>
+    </footer>
+  );
+}
+
+export default Footer;
